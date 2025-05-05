29d99198e4000373cb045df2318124ce464e080a
Geovane Borba <geovanne@live.com>
2025-05-05 18:22:40 -0300

    feat(link): add getLinks route

---
+++ server/src/infra/http/routes/links/get-links-route.ts
@@ -0,0 +1,31 @@
+import { getLinksUseCase } from "@/app/links/use-cases/get-links-use-case";
+import { linkSchema } from "@/app/links/validations/link-schema";
+import { isRight, unwrapEither } from "@/shared/either";
+import {
+  FastifyPluginAsyncZod,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const getLinksRoute: FastifyPluginAsyncZod = async (app) => {
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: "GET",
+    url: "/links",
+    schema: {
+      response: {
+        200: linkSchema.array(),
+        400: z.object({
+          message: z.string(),
+        }),
+      },
+    },
+    handler: async (request, reply) => {
+      const result = await getLinksUseCase();
+
+      if (isRight(result)) {
+        const links = unwrapEither(result);
+        return reply.status(200).send(links);
+      }
+    },
+  });
+};

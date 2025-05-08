cc1894f1d3ec3a834060971ed2d0e6a584fd6af2
Geovane Borba <geovanne@live.com>
2025-05-08 10:55:19 -0300

    feat(web): implement random delay for API requests for testing loading states

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,20 +1,16 @@
+import { randomDelay } from '@/utils/random-delay'
 import axios from 'axios'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
-// const isDelayEnabled = import.meta.env.VITE_ENABLE_API_DELAY === 'true'
+const isDelayEnabled = import.meta.env.VITE_ENABLE_API_DELAY === 'true'
 
-// const randomDelay = (minMs: number, maxMs: number) => {
-//   const delay = Math.floor(Math.random() * (maxMs - minMs + 1)) + minMs
-//   return new Promise((resolve) => setTimeout(resolve, delay))
-// }
+if (isDelayEnabled) {
+  api.interceptors.request.use(async (config) => {
+    await randomDelay(300, 1000) // random delay between 300ms and 1000ms
 
-// if (isDelayEnabled) {
-//   api.interceptors.request.use(async (config) => {
-//     await randomDelay(300, 1000) // random delay between 300ms and 1000ms
-
-//     return config
-//   })
-// }
+    return config
+  })
+}
---
+++ web/src/utils/random-delay.ts
@@ -0,0 +1,5 @@
+export function randomDelay(minMs: number, maxMs: number) {
+  const delay = Math.floor(Math.random() * (maxMs - minMs + 1)) + minMs
+
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}

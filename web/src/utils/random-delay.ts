7a0bdb42a167ee8a91c0f504c2a6433946bb3a27
Geovane Borba <geovanne@live.com>
2025-05-19 19:09:08 -0300

    refactor(utils): remove unused randomDelay

--- web/src/utils/random-delay.ts
+++
@@ -1,5 +0,0 @@
-export function randomDelay(minMs: number, maxMs: number) {
-  const delay = Math.floor(Math.random() * (maxMs - minMs + 1)) + minMs
-
-  return new Promise((resolve) => setTimeout(resolve, delay))
-}

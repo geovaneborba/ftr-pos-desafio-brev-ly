ac4103e05dd8dc7707b8c9cd3080db9e333033f3
Geovane Borba <geovanne@live.com>
2025-05-19 19:07:06 -0300

    refactor(axios): remove API delay interceptor

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,16 +1,5 @@
-import { randomDelay } from '@/utils/random-delay'
 import axios from 'axios'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
-
-const isDelayEnabled = import.meta.env.VITE_ENABLE_API_DELAY === 'true'
-
-if (isDelayEnabled) {
-  api.interceptors.request.use(async (config) => {
-    await randomDelay(300, 1000) // random delay between 300ms and 1000ms
-
-    return config
-  })
-}

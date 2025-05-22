d2fe90f743161541231d1e0a1d7e71bb20f375ef
Geovane Borba <geovanne@live.com>
2025-05-22 20:40:55 -0300

    chore(env): add PostgreSQL to environment schema

--- server/src/env.ts
+++ server/src/env.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "test", "production"])
     .default("development"),
+  POSTGRESQL_USERNAME: z.string(),
+  POSTGRESQL_PASSWORD: z.string(),
+  POSTGRESQL_DATABASE: z.string(),
+  POSTGRESQL_PORT: z.coerce.number().default(5432),
   DATABASE_URL: z.string().url().startsWith("postgresql://"),
   CLOUDFLARE_ACCOUNT_ID: z.string(),
   CLOUDFLARE_ACCESS_KEY_ID: z.string(),
